06a155215192f10b3c4e7c810b7941ac524cf9fc
shoshan <shoshan@example.com>
2024-09-04 19:19:17 +0530

    part7-04-09-2024

--- www/main.js
+++ www/main.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 var siriWave = new SiriWave({
     container: document.getElementById("siri-container"),
     width: 640,
-    height: 200,
+    height: 500,
     style: "ios9",
     amplitude: "1",
     speed: "0.1",
@@ -26,22 +26,23 @@ var siriWave = new SiriWave({
 
   //siri message animation
   $(document).ready(function () {
-    $('.siri message').textillate({
-        loop: true,
-        sync: true,
-        in: {
-            effect: "fadInUp",
-            sync: true
-        },
-        out: {
-            effect: "fadInUp",
-            sync: true
-        }
-    });
-    // mic button click event
+        $('.siri-message').textillate({
+            loop: true,
+            sync: true,
+            in: {
+                effect: "fadInUp",
+                sync: true
+            },
+            out: {
+                effect: "fadeOutUp",
+                sync: true
+            }
+        });
+        // mic button click event
     $("#MicBtn").click(function () { 
         eel.playAssistantSound()
         $("#Oval").attr("hidden", true);
         $("#SiriWave").attr("hidden", false);
+        eel.takecommand()()
     });
 });
