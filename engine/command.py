5dd70cd83e02e9d5a4d2f43b6cc6eaa123f63833
shoshan <shoshan@example.com>
2024-09-04 16:48:04 +0530

    part6-04-09-2024

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -9,5 +10,21 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def takecommand():
+    r =sr.Recognizer()
+    with sr.Microphone() as source:
+        print('listening...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
 
-speak("hurray")
+        audio= r.listen(source,10,6)
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said:{query}")
+    except Exception as e:
+        return ""
+    return query.lower()
+text = takecommand()
+
+speak(text)
