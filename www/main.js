001d8315737dbb8f21b1003e0a801ac45cb17fc7
shoshan <shoshan@example.com>
2024-09-04 15:08:03 +0530

    part3-04-09-2024

--- www/main.js
+++ www/main.js
@@ -10,3 +10,31 @@ $(document).ready(function () {
         }
     });
 });
+
+
+//siri config
+var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 640,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.1",
+    autostart: true
+  });
+
+
+  //siri message animation
+  $('.siri-message').textillate({
+    loop: true,
+    sync: true,
+    in: {
+        effect: "fadeInUp",
+        sync: true,
+    },
+    out: {
+        effect: "fadeOutUp",
+        sync: true,
+    },
+
+});
