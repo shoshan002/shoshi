2eefc623a0fa8ab62aabd6b06711ba2fee10c2ac
shoshan002 <160712433+shoshan002@users.noreply.github.com>
2024-11-07 13:42:08 +0530

    Update helper.py

--- engine/helper.py
+++ engine/helper.py
@@ -5,55 +5,42 @@ import time
 
 
 def extract_yt_term(command):
-    # Define a regular expression pattern to capture the song name
     pattern = r'play\s+(.*?)\s+on\s+youtube'
-    # Use re.search to find the match in the command
     match = re.search(pattern, command, re.IGNORECASE)
-    # If a match is found, return the extracted song name; otherwise, return None
     return match.group(1) if match else None
 
 def extract_spotify_term(command):
-    # Define a regular expression pattern to capture the song name
     pattern = r'play\s+(.*?)\s+on\s+spotify'
-    # Use re.search to find the match in the command
     match = re.search(pattern, command, re.IGNORECASE)
-    # If a match is found, return the extracted song name; otherwise, return None
     return match.group(1) if match else None
 
 
 def remove_words(input_string, words_to_remove):
-    # Split the input string into words
     words = input_string.split()
 
-    # Remove unwanted words
     filtered_words = [word for word in words if word.lower() not in words_to_remove]
 
-    # Join the remaining words back into a string
     result_string = ' '.join(filtered_words)
 
     return result_string
 
 
 
-# key events like receive call, stop call, go back
 def keyEvent(key_code):
     command =  f'adb shell input keyevent {key_code}'
     os.system(command)
     time.sleep(1)
 
-# Tap event used to tap anywhere on screen
 def tapEvents(x, y):
     command =  f'adb shell input tap {x} {y}'
     os.system(command)
     time.sleep(1)
 
-# Input Event is used to insert text in mobile
 def adbInput(message):
     command =  f'adb shell input text "{message}"'
     os.system(command)
     time.sleep(1)
 
-# to go complete back
 def goback(key_code):
     for i in range(6):
         keyEvent(key_code)
